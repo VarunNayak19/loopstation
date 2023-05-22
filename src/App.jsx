f4c095cadaafee9fe02d44acd5b1b73dc63c47fa
VarunNayak19 <varun.nayak@robosoftin.com>
2023-05-22 16:32:47 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './index.css'
 import HeroSection from './components/HeroSection/HeroSection'
 import FeatureSection from './components/FeatureSection/FeatureSection'
-import Creations from './components/Creations/creations'
+import Creations from './components/Creations/Creations'
 import Footer from './components/Footer/Footer'
 
 function App() {
